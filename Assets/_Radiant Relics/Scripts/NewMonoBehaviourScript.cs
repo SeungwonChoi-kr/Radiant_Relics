ec11816b484bee0d77f3ef371254f6114fc9c1f4
a5153203 <a5153203@gmail.com>
2025-11-20 04:22:16 +0900

    Add MainMenu & Setting Scenes, Images and scripts
    MainMenu: 배경, 제목, 버튼(플레이, 설정, 나가기)
    Setting: 볼륨 설정 메뉴
    Exit: 게임 종료
    Scripts: 메인메뉴, 설정화면, 메인Scene 전환

---
+++ Assets/_Radiant Relics/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("게임 종료");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("메인메뉴로 돌아가기");
+    }
+}
---
+++ Assets/_Radiant Relics/Scripts/volumecontroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    public AudioSource bgmSource;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        // 저장된 볼륨 불러오기
+        float volume = PlayerPrefs.GetFloat("Volume", 1f);
+        volumeSlider.value = volume;
+        bgmSource.volume = volume;
+
+        // 슬라이더 값 변경 시 즉시 BGM 반영
+        volumeSlider.onValueChanged.AddListener(UpdateVolume);
+    }
+
+    void UpdateVolume(float value)
+    {
+        bgmSource.volume = value;
+        PlayerPrefs.SetFloat("Volume", value);
+    }
+}

32679c822f6e76592950939da7e60cd77d7c8b1b
SeungwonChoi-kr <choiswon2002@gmail.com>
2025-11-20 00:06:58 +0900

    Refactor: 건물 깔끔하게 배치, NevMesh_Blocker 추가하여 NPC가 건물 내부로 들어가지 않도록 수정, README 수정

---
+++ Assets/_Radiant Relics/Scripts/NpcRandomIdle.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class NpcRandomIdle : MonoBehaviour
+{
+    private Animator animator;
+
+    private float npcSpeed;
+    private readonly int _MoveSpeedHash = Animator.StringToHash("MoveSpeed");
+
+    private int nextIdleIndex = 1;
+    private readonly int _IdleRandomIndex = Animator.StringToHash("IdleRandomIndex");
+
+    private bool isIdleSet = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator == null )
+        {
+            Debug.LogError("[NpcRandomIdle] 애니메이터 연결 안 됨");
+        }
+
+        RandomizeNextIdle();
+    }
+
+    void Update()
+    {
+        if (animator == null) return;
+
+        npcSpeed = animator.GetFloat(_MoveSpeedHash);
+
+        if (npcSpeed > 0.1f && !isIdleSet)
+        {
+            RandomizeNextIdle();
+            isIdleSet = true;
+        }
+        else if (npcSpeed < 0.1f)
+        {
+            isIdleSet = false;
+        }
+    }
+
+    private void RandomizeNextIdle()
+    {
+        nextIdleIndex = Random.Range(1, 3); // 1 아니면 2
+
+        animator.SetInteger(_IdleRandomIndex, nextIdleIndex);
+    }
+}

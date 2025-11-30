965adb0d9798a366cead1f8f2a88c36ec32f7b9d
최승원 <121798929+SeungwonChoi-kr@users.noreply.github.com>
2025-12-01 00:41:46 +0900

    Merge: from 'feature/pop-resources' to 'develop'
    Add: 자원 프리팹에서 주괴 오브젝트들 팝하고 튀어나오는 효과
    Add: 주괴 오브젝트 F키 눌러서 줍는 기능
    
    ps. PickUp 스크립트에서 인벤토리에 주괴 넣는 코드 넣어줘야 함

---
+++ Assets/_Radiant Relics/Scripts/PickUpItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    [Header("Item Info")]
+    public string itemName; // 디버깅용
+
+    public void OnInteract()
+    {
+        if (itemName != null)
+        {
+            Debug.Log("[PickUpItem] " + itemName + " 획득");
+        }
+
+        // InventoryManager.Instance.AddItem(itemName, 1);  인벤토리 코드에서 추가하는 함수가 있는지 모르겠지만 이런 식으로 추가하면 될 듯
+
+        // 오브젝트 삭제
+        Destroy(gameObject);
+    }
+}
